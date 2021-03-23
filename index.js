66083f01a15e3e324a279e14c8cc070f36dcfede
Alexandr Balasyan <mywebstreet@gmail.com>
2021-03-23 15:26:24 +0300

    upgraded deps

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 module.exports = (context, options)  => {
   options = Object.assign({ 
     usePresetEnv: true,
-    useAsyncAwait: true,
+    useTransformAsync: true,
+    useTransformRuntime: true,
     useDynamicImport: true,
     useClassProperties: true,
     useDecorators: true,
@@ -19,8 +20,9 @@ module.exports = (context, options)  => {
   const plugins = [];
 
   options.usePresetEnv && presets.push([require("@babel/preset-env"), options.presetEnvOptions]);
-  options.useAsyncAwait && plugins.push([require('@babel/plugin-syntax-dynamic-import'), options.pluginAsyncAwaitOptions]);
-  options.useDynamicImport && plugins.push([require('@babel/plugin-transform-async-to-generator'), options.pluginDynamicImportOptions]);
+  options.useTransformAsync && plugins.push([require('@babel/plugin-transform-async-to-generator'), options.pluginTransformAsyncOptions]);
+  options.useTransformRuntime && plugins.push([require('@babel/plugin-transform-runtime'), options.pluginTransformRuntimeOptions]);  
+  options.useDynamicImport && plugins.push([require('@babel/plugin-syntax-dynamic-import'), options.pluginDynamicImportOptions]);
   options.useDecorators && plugins.push([require('@babel/plugin-proposal-decorators'), options.pluginDecoratorsOptions]);
   options.useClassProperties && plugins.push([require('@babel/plugin-proposal-class-properties'), options.pluginClassPropertiesOptions]);
   
